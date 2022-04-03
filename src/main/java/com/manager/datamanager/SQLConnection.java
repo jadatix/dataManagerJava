3ec65e68f3d67c607ef8f0aa6ff8aa25dbfbcf09
jadatix <andrijlasenko@gmail.com>
2022-04-03 19:48:08 +0300

    add new Teacher to database function(not finished and tested)

--- src/main/java/com/manager/datamanager/SQLConnection.java
+++ src/main/java/com/manager/datamanager/SQLConnection.java
@@ -1,12 +1,14 @@
 package com.manager.datamanager;
 
+import entities.Teacher;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class SQLConnection {
-    public static void main(String[] args) {
+    public void testing(String[] args) {
         try {
             Connection connection;
             Class.forName("org.postgresql.Driver");
@@ -20,14 +22,48 @@ public class SQLConnection {
             Statement  statement = connection.createStatement();
             ResultSet res = statement.executeQuery("select * from test1table");
 
-//            while (res.next()){
-//                System.out.println(res.getString("name"));
-//            }
+            while (res.next()){
+                System.out.println(res.getString("name"));
+            }
             statement.close();
             connection.close();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static int addTeacher(Teacher teacher){
+        int statusCode=0;
+        try{
+            Connection connection;
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/shop", "postgres", "");
+            if (connection != null) {
+                System.out.println("Connection: we did it :)");
+            } else {
+                System.out.println("Connection: connection failed :(");
+                statusCode=1;
+            }
+
+            Statement  statement = connection.createStatement();
+            ResultSet res = statement.executeQuery("INSERT INTO Teacher(id,name,lastname,email,password,gender,age,headOf,subject)" +
+                    "VALUES (NULL,"+teacher.getName()+
+                    ","+teacher.getLastname()+
+                    ","+teacher.getEmail()+
+                    ","+teacher.getPassword()+
+                    ","+teacher.getGender()+
+                    ","+teacher.getAge()+
+                    ","+teacher.getHeadOf()+
+                    ","+teacher.getSubject().stream().map(el -> el+" ")+
+                    ")");// TODO: subject find out how to deal with (create test)
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+
+        return statusCode;
     }
 }
