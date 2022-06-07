616674ed8110058ef6678e03487b8cae0979fd95
jadatix <andrijlasenko@gmail.com>
2022-06-07 14:29:36 +0300

    add application icon

--- src/main/java/com/manager/datamanager/Main.java
+++ src/main/java/com/manager/datamanager/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,6 +18,12 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("admin.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
+            try {
+                stage.getIcons().add(new Image(Main.class.getResourceAsStream("/images/icons8-students-64.png")));
+            } catch (NullPointerException err){
+                System.out.println(err.getMessage());
+            }
+
             stage.show();
         }catch (Exception e){
            System.out.println(e);
